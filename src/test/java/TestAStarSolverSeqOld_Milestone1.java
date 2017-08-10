0a5dee67adab24d5758f0526b6d8f2a8a52226ef
will <willjoemolloy@gmail.com>
2017-08-10 20:12:33 +1200

    Added test with an input that utilises >2 processing cores in its optimal schedule. This currently FAILS as our solver only makes use of 2 cores (the first and last one). Please see the @Ignore test in TestAStarSolverSeqOld.

--- src/test/java/TestAStarSolverSeqOld_Milestone1.java
+++ src/test/java/TestAStarSolverSeqOld_Milestone1.java
@@ -9,6 +9,7 @@ import Parser.InputParser;
 import Parser.VertexCtor;
 import SolverOld.AStarSolver;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 
@@ -24,7 +25,7 @@ import static junit.framework.TestCase.assertEquals;
  */
 public class TestAStarSolverSeqOld_Milestone1 {
 
-    private static final int PROCESSOR_COUNT = 2;
+    private static int PROCESSOR_COUNT;
     private static final String TEST_FILES_PATH = "src/test/resources/",
             TEST_SOLVER = "TestSolver/",
             MILESTONE_1_INPUT = "input-graphs-milestone1/",
@@ -35,10 +36,15 @@ public class TestAStarSolverSeqOld_Milestone1 {
 
     @Before
     public void setup() {
+        PROCESSOR_COUNT = 2; // Override in individual tests if needed.
         graph = new Graph<Vertex, EdgeWithCost<Vertex>>();
         parser = new InputParser<Vertex, EdgeWithCost<Vertex>>(new VertexCtor(), new EdgeCtor());
     }
 
+    /**
+     * This test ensures the schedule is valid as the solver may place nodes on other cores in parallel when it
+     * cannot. The optimal schedule here only uses 1 core.
+     */
     @Test
     public void testStraightLine(){
         graph = parser.doParseAndFinaliseGraph(TEST_FILES_PATH + TEST_SOLVER + "input_straightline_4nodes.dot");
@@ -50,6 +56,9 @@ public class TestAStarSolverSeqOld_Milestone1 {
         assertEquals(expected, actual);
     }
 
+    /**
+     * Input was added to canvas; Thursday 20170810
+     */
     @Test
     public void testNodes_7_OutTree() {
         graph = parser.doParseAndFinaliseGraph(TEST_FILES_PATH + MILESTONE_1_INPUT + "Nodes_7_OutTree.dot");
@@ -58,8 +67,24 @@ public class TestAStarSolverSeqOld_Milestone1 {
 
         String actual = GraphExporter.exportGraphToString(graph);
         String expected = FileUtils.readFileToString(TEST_FILES_PATH + MILESTONE_1_OUTPUT + "output_Nodes_7_OutTree.dot");
-        System.out.println(actual);
 
+        assertEquals(expected, actual);
+    }
+
+    /**
+     * This test ensures multiple cores are being used when they are required for the optimal schedule as there are no
+     * dependencies between nodes.
+     */
+    @Ignore
+    public void test8Nodes0Edges(){
+        PROCESSOR_COUNT = 8;
+        graph = parser.doParseAndFinaliseGraph(TEST_FILES_PATH + TEST_SOLVER + "input_8nodes_0edges.dot");
+        solver = new AStarSolver(graph, PROCESSOR_COUNT); // Must construct solver only after graph has been parsed in.
+        solver.doSolve();
+
+        String actual = GraphExporter.exportGraphToString(graph);
+        String expected = FileUtils.readFileToString(TEST_FILES_PATH + TEST_SOLVER + "output_8nodes_0edges.dot");
+        System.out.println(actual);
         assertEquals(expected, actual);
     }
 
