e324abe6d35d367ae9206b7db0281bfe7a66c9b2
will <willjoemolloy@gmail.com>
2017-08-23 22:45:07 +1200

    Added test case that breaks excluding startTimes from the SearchState equals() method for the A* solver.

--- src/test/java/TestSolversSequential.java
+++ src/test/java/TestSolversSequential.java
@@ -137,7 +137,7 @@ public class TestSolversSequential {
     }
 
     /*
-     * Edge cases to break our optimisations
+     * Edge cases to break our optimisations - they currently do not. Online example does however.
      */
     @Test
     public void testExcludeProcessorsEdgesWithZeroCost(){
@@ -166,13 +166,53 @@ public class TestSolversSequential {
 
     /**
      * https://www.math.unl.edu/~mbrittenham2/classwk/203f07/quiz/203words7.pdf
+     * Note they don't use edge costs (the numbers they have represent a task priority)
+     * Therefore the answer they give is incorrect, it can be done in 2 less time units for our rules.
      */
     @Test
-    public void test14NodesWords7dvi2Core(){
+    public void test14NodesUoN2Core(){
+        solver = tester.doTest(2, new File(TEST_FILE_PATH + TEST_SOLVER_PATH + "input_14Nodes_203words7dvi_0edgecost.dot"));
+        log.debug(GraphExporter.exportGraphToString(solver.getGraph()));
+        assertEquals(72, solver.getFinalTime());
+    }
+
+    @Test
+    public void test14NodeUoN3Core(){
+        solver = tester.doTest(3, new File(TEST_FILE_PATH + TEST_SOLVER_PATH + "input_14Nodes_203words7dvi_0edgecost.dot"));
+        log.debug(GraphExporter.exportGraphToString(solver.getGraph()));
+        assertEquals(57, solver.getFinalTime());
+    }
+
+    /*
+     * Ensures the optimal solution with unlimited cores doesn't beat the 3 core optimal solution
+     * Putting more cores will cause DFS to take forever.
+     */
+    @Test
+    public void test14NodeUoN4Core(){
+        solver = tester.doTest(4, new File(TEST_FILE_PATH + TEST_SOLVER_PATH + "input_14Nodes_203words7dvi_0edgecost.dot"));
+        log.debug(GraphExporter.exportGraphToString(solver.getGraph()));
+        assertEquals(57, solver.getFinalTime());
+    }
+
+    /**
+     * https://www.math.unl.edu/~mbrittenham2/classwk/203f07/quiz/203words7.pdf
+     * Note: not all the edges were added; I made a mistake adding them and accidentally found a testcase that breaks our solution!!
+     * Added edge costs (I thought that's what they were at first)
+     * This test breaks the 'exclude "startTimes"' in SearchState class.
+     *
+     * The expected finalTime is 96 (NOT CONFIRMED), "exclude startTimes" gives 98
+     * This is because with "exclude startTimes" the priority queue is adding a state that is treated as equal (incorrectly)
+     * to the state(s) that are required to produce the optimal schedule. Therefore the pre state to the optimal state is
+     * not considered.
+     */
+    @Test
+    public void test14NodesUoN2CoreWithEdgeCost(){
         solver = tester.doTest(2, new File(TEST_FILE_PATH + TEST_SOLVER_PATH + "input_14Nodes_3CoreOptimal.dot"));
         log.debug(GraphExporter.exportGraphToString(solver.getGraph()));
-        assertEquals(74, solver.getFinalTime());
+        assertEquals(96, solver.getFinalTime());
     }
+
+
 
 
 }
