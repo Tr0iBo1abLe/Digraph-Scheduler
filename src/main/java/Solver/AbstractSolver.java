ab9f6e301819d06466ad10f943ba7f3658d83449
will <willjoemolloy@gmail.com>
2017-08-26 11:33:56 +1200

    Add getGraph() to solvers so output can be compared with graph exporter.

--- src/main/java/Solver/AbstractSolver.java
+++ src/main/java/Solver/AbstractSolver.java
@@ -24,6 +24,7 @@ import java.util.Timer;
  */
 @Data
 abstract public class AbstractSolver implements ISolver {
+    @Getter
     protected Graph<Vertex, EdgeWithCost<Vertex>> graph;
     protected int processorCount;
     @Getter
